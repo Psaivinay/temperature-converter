0420603f0f6e2e075b7744f677997fa017c93d8c
Psaivinay <108043889+Psaivinay@users.noreply.github.com>
2023-07-01 17:25:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function convertToFahrenheit() {
+    let celsius = document.getElementById("celsius").value;
+    let fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit;
+  }
+  
+  function convertToCelsius() {
+    let fahrenheit = document.getElementById("fahrenheit").value;
+    let celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById("celsius").value = celsius;
+  }
+  
